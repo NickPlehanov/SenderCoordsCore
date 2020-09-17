d15c21c05154a5f779f73231632c83b18bab4a21
Nick Plehanov <nick.plehanov74@gmail.com>
2020-09-17 18:02:52 +0500

    Добавьте файлы проекта.

---
+++ Models/Contexts/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SenderCoordCore.Models.Contexts {
+    public class CoordsContext : DbContext {
+        public CoordsContext(){}
+        public CoordsContext(DbContextOptions<CoordsContext> options) : base(options) { }
+
+        public virtual DbSet<Coords> Coords { get; set; }
+    }
+}
---
+++ Models/Coords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SenderCoordCore.Models {
+    [Table("Coords")]
+    public class Coords {
+        [Key]
+        public Guid coo_RecordID { get; set; }
+        public string coo_imei { get; set; }
+        public string coo_longitude { get; set; }
+        public string coo_latitude { get; set; }
+    }
+}
